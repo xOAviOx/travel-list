d6964947cd1040968338ee498ea0da228d9fa21b
xOAviOx <avishuklacode@gmail.com>
2025-03-05 00:06:36 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
-export default function App(){
-  
+export default function App() {}
+
+function Logo() {
+  return <h1> Far Away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      You have X items on your list, and you already packed X (X%)
+    </footer>
+  );
 }
