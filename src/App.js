e72a5dd54a37d0f604964cc800e15942ea0e1445
xOAviOx <avishuklacode@gmail.com>
2025-03-05 21:47:54 +0530

    Layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-export default function App() {}
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
 function Logo() {
-  return <h1> Far Away</h1>;
+  return <h1>🌴Far Away🌴</h1>;
 }
 function Form() {
   return (
     <div className="add-form">
-      <h3>What do you need for your trip?</h3>
+      <h3>What do you need for your 😊 trip?</h3>
     </div>
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>X</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
-      You have X items on your list, and you already packed X (X%)
+    <footer className="stats">
     </footer>
   );
 }
