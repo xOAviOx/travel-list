51a6376631cc24ba54e288c54b540f580e3f6b08
xOAviOx <avishuklacode@gmail.com>
2025-03-06 11:14:30 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -20,7 +21,7 @@ function Logo() {
 function Form() {
   return (
     <div className="add-form">
-      <h3>What do you need for your 😊 trip?</h3>
+      <h3>What do you need for your trip?😊</h3>
     </div>
   );
 }
@@ -35,21 +36,20 @@ function PackingList() {
     </div>
   );
 }
-
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>X</button>
+      <button>❌</button>
     </li>
   );
 }
-
 function Stats() {
   return (
     <footer className="stats">
+      <em>You have X items on your list, and you already packed X</em>
     </footer>
   );
 }
